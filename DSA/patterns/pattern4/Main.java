face7a1b5a65e72e1f0de5f337aab91e58437f14
aniketkr <aniket.kumar@nupay.co.in>
2023-01-30 00:19:59 +0530

    pattern 4

---
+++ DSA/patterns/pattern4/Main.java
@@ -0,0 +1,22 @@
+package pattern4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("enter the value of n : ");
+        int n = scn.nextInt();
+        scn.close();
+
+        for(int i=0;i<n;i++){
+            for(int space=0;space<i;space++){
+                System.out.print("  ");
+            }
+            for(int star=0;star<n-i;star++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
