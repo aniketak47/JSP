baf540b1c11762e7d127dd436f2ccbf7ef77038c
aniketak47 <aniketkr20012019@gmail.com>
2023-02-06 11:21:56 +0530

    any base addition

---
+++ DSA/anyBaseAddition/Main.java
@@ -0,0 +1,53 @@
+package DSA.anyBaseAddition;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter the value of base : ");
+        int b = scn.nextInt();
+        System.out.println("enter the first value n1 : ");
+        int n1 = scn.nextInt();
+        System.out.println("enter the second value n2 : ");
+        int n2 = scn.nextInt();
+        scn.close();
+
+        int n1InDecimal = anyBaseToDecimal(n1, b);
+        int n2InDecimal = anyBaseToDecimal(n2, b);
+
+        int n = n1InDecimal + n2InDecimal;
+        int ans = decimalToAnyBase(n, b);
+        System.out.println("ans is : "+ans);
+    }
+
+    public static int anyBaseToDecimal(int n, int b) {
+        int ans = 0;
+
+        int power = 1;
+        while(n>0){
+            int rem = n%10;
+            n = n/10;
+
+            ans = ans + rem*power;
+            power = power * b;
+        }
+
+        return ans;
+    }
+
+    public static int decimalToAnyBase(int n, int b) {
+        int ans = 0;
+
+        int power = 1;
+        while(n>0){
+            int rem = n%b;
+            n = n/b;
+
+            ans = ans + rem*power;
+            power = power*10;
+        }
+
+        return ans;
+    }
+}
